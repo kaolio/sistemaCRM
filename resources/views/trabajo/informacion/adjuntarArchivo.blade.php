2cd1c87df23a8e72664048ffa1124c4195ef6260
kaolio <49197208+kaolio@users.noreply.github.com>
2022-07-01 11:53:38 -0400

    cambios agregados 13.2

--- resources/views/trabajo/informacion/adjuntarArchivo.blade.php
+++ resources/views/trabajo/informacion/adjuntarArchivo.blade.php
@@ -4,8 +4,8 @@
         <form action="{{url('/trabajos/detalle/subir')}}" method="POST" enctype="multipart/form-data">
             @csrf
             <div class="text-center">
-                <input class="form-control" id="file-upload" name="file-upload" type="file"/>
-                <input type="submit" class="btn btn-sm btn-block btn-success" value="">
+                <input class="form-control" id="file-upload" name="file-upload" type="file" style="width: 250px;">
+                <input type="submit" class="btn btn-sm btn-success" value="Subir Archivo">
              </div>
         </form>
     </div>
